165f70be2dd9bd52cf53e1910fa37103ac95116f
lengfelj <joe.lengfellner@gmail.com>
2014-01-24 23:40:53 -0500

    First Commit

---
+++ Miles Per Day.R
@@ -0,0 +1,13 @@
+library("ggplot2", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+FullDS <- read.csv("~/Dropbox/R/CitiBike/FullDS.csv")
+
+#Convert the date column to a date type
+FullDS$Date <- as.Date(FullDS$Date, "%m/%d/%y")
+
+
+
+#Simple line plot of miles per day
+a <- ggplot(FullDS, aes(x=Date, y=Miles))
+a <- a+geom_line()
+a <- a+ geom_smooth()
+a  
